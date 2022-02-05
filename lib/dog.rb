88f969656232be8037a5b0f04d8d569129f794de
Vikalp Malhotra <kimboho@gmail.com>
2022-02-05 17:43:56 -0500

    commit tree 8a1adb0f06b63a01826dadd675bbf574397a9d29

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+    def name= (doggo)
+        @name = doggo
+    end
+    def name 
+        @name
+    end
+    def breed= (type)
+        @breed = type
+    end
+    def breed
+        @breed
+    end
 
 end
+
+navi = Dog.new
+
+navi.name = "Navi"
+navi.breed = 'pitbull'
+
+puts navi.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
+ def name= (bob)
+    @name= bob
+ end
+ def name
+    @name
+ end
 
+ def job= (programmer)
+    @job = programmer
+ end
+ 
+ def job
+    @job 
+ end
 end
